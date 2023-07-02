fcf13985017220327e45742e8d3d90a6ee550fed
SaidosHits <137960489+SaidosHits@users.noreply.github.com>
2023-07-02 13:22:17 +0100

    Update App.js
    fix problem respone not found data

--- src/App.js
+++ src/App.js
@@ -7,158 +7,122 @@ import mist from "./images/mist.png"
 import rain from "./images/rain.png"
 import snow from "./images/snow.png"
 
-
-
-
 const App = () => {
-const [imagew , setimages] = useState(Clear)
-const cityref = useRef()
-const [location , setcity] = useState('france')
-const [weather , setweather] = useState({
-  "coord": {
-      "lon": -87.4534,
-      "lat": 40.9462
-  },
-  "weather": [
-      {
-          "id": 804,
-          "main": "Clouds",
-          "description": "overcast clouds",
-          "icon": "04d"
+  const [imagew, setImages] = useState(Clear);
+  const cityRef = useRef();
+  const [location, setCity] = useState('france');
+  const [weather, setWeather] = useState(null);
+  const [notFound, setNotFound] = useState(false);
+
+  const weatherFetch = () => {
+    let weContainer = document.querySelector('.weather-container');
+    weContainer.style.height = '500px';
+    const apiKey = '652006db3af19c5be21c71b9f74e9b3a';
+    const city = location;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          setNotFound(true);
+          
+        } else {
+          setNotFound(false);
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (!notFound) {
+          setWeather(data);
+        }
+      })
+  };
+
+  const changeSrc = () => {
+    if (weather) {
+      switch (weather.weather[0].main) {
+        case 'Clear':
+          setImages(Clear);
+          break;
+        case 'Rain':
+          setImages(rain);
+          break;
+        case 'Snow':
+          setImages(snow);
+          break;
+        case 'Clouds':
+          setImages(Cloud);
+          break;
+        case 'Haze':
+          setImages(mist);
+          break;
+        default:
+          setImages(Clear);
+          break;
       }
-  ],
-  "base": "stations",
-  "main": {
-      "temp": 27.29,
-      "feels_like": 30.73,
-      "temp_min": 26.98,
-      "temp_max": 28.85,
-      "pressure": 1012,
-      "humidity": 83,
-      "sea_level": 1012,
-      "grnd_level": 989
-  },
-  "visibility": 10000,
-  "wind": {
-      "speed": 2.34,
-      "deg": 274,
-      "gust": 4.07
-  },
-  "clouds": {
-      "all": 95
-  },
-  "dt": 1688169802,
-  "sys": {
-      "type": 2,
-      "id": 2009751,
-      "country": "US",
-      "sunrise": 1688120455,
-      "sunset": 1688174742
-  },
-  "timezone": -18000,
-  "id": 4923796,
-  "name": "Morocco",
-  "cod": "404"
-})
-const weatherfetch = () =>{
-  let wecontainer = document.querySelector('.weather-container')
-  let notfoundcon = document.querySelector(".not-found")
-  wecontainer.style.height = "500px"
-  const apikey = '652006db3af19c5be21c71b9f74e9b3a'
-  const city = location
-  const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}` 
-  fetch(url)
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-    setweather(data)
-    if(data.cod === "404"){
-     wecontainer.style.display= "none"
-     notfoundcon.style.display='flex'
     }
-  })
-}
-
-const changesrc = ()=>{
-  switch(weather.weather[0].main){
-    case 'Clear':
-      setimages(Clear)
-      break;
-
-  case 'Rain':
-     setimages(rain)
-      break;
-
-  case 'Snow':
-      setimages(snow)
-      break;
-
-  case 'Clouds':
-   setimages(Cloud)
-      break;
-
-  case 'Haze':
-      setimages(mist)
-      break;
-
-  default:
-      setimages(Clear)
-  }
-  
-}
-useEffect(()=>{
-  changesrc()
-},[weather])
-
-
-
+  };
 
   return (
     <>
-    <div className='weather-container'>
-      <div className='search-from'>
-        <div className='sr-form'>
-        <i class="fa-solid fa-location-dot"></i>
-        <input ref={cityref} onChange={(event) => setcity(event.target.value)} type='text' placeholder='Type location'></input>
-        <button onClick={weatherfetch}><i class="fa-solid fa-magnifying-glass"></i></button>
-        </div>
-        <div className='not-found'>
-          <img src={e404}/>
-          <div className='not-descraption'>
-          <p>Your location is not found /:</p>
+      <div className="weather-container">
+        <div className="search-from">
+          <div className="sr-form">
+            <i className="fa-solid fa-location-dot"></i>
+            <input
+              ref={cityRef}
+              onChange={(event) => setCity(event.target.value)}
+              type="text"
+              placeholder="Type location"
+            />
+            <button onClick={weatherFetch}>
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
           </div>
-        </div>
-        <div className='weather-search'>
-          <img src={imagew}/>
-          <p class="temperature">
-            <h1><span>{weather.main.temp}°C</span></h1>
-          </p>
-            <p class="description">
-              <h2>{weather.weather[0].description}</h2>
-            </p>
-        </div>
-        <div class="weather-details">
-            <div class="humidity">
-                <i class="fa-solid fa-water"></i>
-                <div class="text">
-                    <span>{weather.main.humidity}%</span>
-                    <p>Humidity</p>
-                </div>
+          {notFound && (
+            <div className="not-found">
+              <img src={e404} alt="404 Error" />
+              <div className="not-description">
+                <p>Location not found</p>
+              </div>
             </div>
-            <div class="wind">
-                <i class="fa-solid fa-wind"></i>
-                <div class="text">
+          )}
+          {weather && !notFound && (
+            <div className="weather-search">
+              <img src={imagew} alt="Weather" />
+              <p className="temperature">
+                <h1>
+                  <span>{weather.main.temp}°C</span>
+                </h1>
+              </p>
+              <p className="description">
+                <h2>{weather.weather[0].description}</h2>
+              </p>
+            </div>
+          )}
+          {weather && !notFound && (
+            <div className="weather-details">
+              <div className="humidity">
+                <i className="fa-solid fa-water"></i>
+                <div className="text">
+                  <span>{weather.main.humidity}%</span>
+                  <p>Humidity</p>
+                </div>
+              </div>
+              {weather && (
+                <div className="wind">
+                  <i className="fa-solid fa-wind"></i>
+                  <div className="text">
                     <span>{weather.wind.speed}km/s</span>
                     <p>Wind Speed</p>
+                  </div>
                 </div>
+              )}
             </div>
+          )}
         </div>
       </div>
-    </div>
-    
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
